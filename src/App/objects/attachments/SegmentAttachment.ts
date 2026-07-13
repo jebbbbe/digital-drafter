f17b855678f04528e22d3564caecf072062dd83d
jebbbbe <74994218+jebbbbe@users.noreply.github.com>
2026-07-13 14:32:50 -0400

    SegmentAttachment

--- src/App/objects/attachments/SegmentAttachment.ts
+++ src/App/objects/attachments/SegmentAttachment.ts
@@ -1,16 +1,109 @@
+import * as THREE from "three"
 import { SectionCutter } from "./SectionCutter"
+import { InteractiveObject } from "../../selection/SelectionObject"
+import { drafter, controllers } from "../../AppContext"
+import type { GizmoSettings } from "../../selection/ThreeControllersManager"
+import type { PanelSettings } from "../../../components/Leva/LevaStore"
+import { updatePanel } from "../../../components/Leva/LevaStore"
+import { moveSegmentToPosition } from "../../controls/move"
+import { deSelectAll } from "../../controls/interaction"
+import { deleteSegment } from "../../controls/section"
+import type { SectionSegment } from "../../selection"
 
-export type SegmentAttachment = {
+const _segmentMidPoint = new THREE.Vector3()
+const _segmentDirection = new THREE.Vector3()
+const _segmentQuaternion = new THREE.Quaternion()
+const _segmentZAxis = new THREE.Vector3(0, 0, -1)
+const _zeroVec3 = new THREE.Vector3()
+
+// export type SegmentAttachment = {
+//     object: SectionCutter
+//     index: number
+// }
+
+export class SegmentAttachment extends InteractiveObject {
     object: SectionCutter
     index: number
+    constructor(object: SectionCutter, index: number) {
+        super()
+        this.object = object
+        this.index = index
+    }
+    get kind(): "SectionSegment" {
+        return "SectionSegment"
+    }
+
+    override move(_startHit: THREE.Vector3) {
+        return undefined
+    }
+
+    override getCenter() {
+        const line = this
+        const [a, b] = drafter.sectionCutter.getSegmentAsVector(line.index)
+        _segmentMidPoint.addVectors(a, b).multiplyScalar(0.5)
+        return _segmentMidPoint
+    }
+
+    override gizmoSetup(settings: Partial<GizmoSettings>) {
+        const line = this
+        const [a, b] = drafter.sectionCutter.getSegmentAsVector(line.index)
+        _segmentMidPoint.addVectors(a, b).multiplyScalar(0.5)
+        _segmentDirection.subVectors(b, a)
+        if (_segmentDirection.lengthSq() === 0) {
+            _segmentQuaternion.identity()
+        } else {
+            _segmentQuaternion.setFromUnitVectors(
+                _segmentZAxis,
+                _segmentDirection.normalize()
+            )
+        }
+
+        const defaultGizmo: GizmoSettings = {
+            anchor: _zeroVec3,
+            center: _segmentMidPoint,
+            quaternion: _segmentQuaternion,
+            preset: "translate1d",
+        }
+        settings = { ...defaultGizmo, ...settings }
+        controllers.setGizmoSettings(settings as GizmoSettings)
+    }
+
+    override panelSetup(settings: Partial<PanelSettings>) {
+        const line = this
+        const [a, b] = drafter.sectionCutter.getSegmentAsVector(line.index)
+        _segmentMidPoint.addVectors(a, b).multiplyScalar(0.5)
+
+        const defaultPanel = {
+            position: _segmentMidPoint,
+            rotation: { x: 0, y: 0 },
+            scale: 1.0,
+            usePosition: true, //should update vals but be unusable...
+            useRotation: false,
+            useScale: false,
+            useButtons: false,
+        }
+        settings = { ...defaultPanel, ...settings }
+        console.log(settings)
+        updatePanel(settings as PanelSettings)
+    }
+
+    override gizmoListener(position = controllers.getGizmoPosition()) {
+        moveSegmentToPosition(this as unknown as SectionSegment, position)
+    }
+
+    override delete() {
+        deSelectAll()
+        deleteSegment(this as unknown as SectionSegment)
+    }
+
+    override setSelected(isSelected: boolean) {
+        console.warn("not implemented Select for ", this)
+    }
 }
 
 export function createSegmentAttachment(
     object: SectionCutter,
     index: number
 ): SegmentAttachment {
-    return {
-        object,
-        index,
-    }
+    return new SegmentAttachment(object, index)
 }
