9bfcfe361631a8230e897d53232f670a818edf81
jebbbbe <74994218+jebbbbe@users.noreply.github.com>
2026-08-19 17:03:12 -0400

    chunk fix

--- src/App/objects/materials/glsl/chunk/treeShaderChunk.ts
+++ src/App/objects/materials/glsl/chunk/treeShaderChunk.ts
@@ -1,10 +1,10 @@
 import * as THREE from "three"
 
-import uniformTree from "./glsl/chunk/uniformTree.glsl?raw"
-import treeFuncitons from "./glsl/chunk/treeFuncitons.glsl?raw"
-import treeMain from "./glsl/chunk/treeMain.glsl?raw"
-import uniformFold from "./glsl/chunk/uniformFoldLine.glsl?raw"
-import instancedLine from "./glsl/chunk/instancedLine.glsl?raw"
+import uniformTree from "./uniformTree.glsl?raw"
+import treeFuncitons from "./treeFuncitons.glsl?raw"
+import treeMain from "./treeMain.glsl?raw"
+import uniformFold from "./uniformFoldLine.glsl?raw"
+import instancedLine from "./instancedLine.glsl?raw"
 
 const replaceGLSL = {
     // oldString:newString
